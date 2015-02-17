21c3792e9960eb5a93a44e0d22028fba3a40951f
Bryce van Dyk <bryce@vandyk.net.nz>
2015-02-17 15:43:14 +1300

    Added frequency traint impl for strings
    Also messed around with the slice impl. Not sure if I'm using lifetimes
    correctly, but eghg!

--- src/frequency_analysis.rs
+++ src/frequency_analysis.rs
@@ -4,16 +4,31 @@ use std::collections::btree_map::{BTreeMap, Entry};
 trait FrenquencyAnalysable {
 	type Item : Ord;
 
-	fn frequencies(&self) -> BTreeMap<&Self::Item, usize>;
+	fn frequencies(&self) -> BTreeMap<Self::Item, usize>;
 }
 
 
-impl<T : Ord> FrenquencyAnalysable for [T] {
-	type Item = T;
+impl<'a, T : Ord> FrenquencyAnalysable for [T] {
+	type Item = &'a T;
 
-	fn frequencies<'a>(self : &'a [T]) -> BTreeMap<&'a T, usize> {
-		let mut frequencies : BTreeMap<&T, usize> = BTreeMap::new();
+	fn frequencies<'b>(self : &'b [T]) -> BTreeMap<&'b T, usize> {
+		let mut frequencies : BTreeMap<&'b T, usize> = BTreeMap::new();
 		for item in self.iter() {
+			match frequencies.entry(item) {
+				Entry::Vacant(entry) => { entry.insert(1); },
+				Entry::Occupied(mut entry) => *entry.get_mut() += 1,
+			}
+		}
+		return frequencies;
+	}
+}
+
+impl FrenquencyAnalysable for str {
+	type Item = char;
+
+	fn frequencies(self : &str) -> BTreeMap<char, usize> {
+		let mut frequencies : BTreeMap<char, usize> = BTreeMap::new();
+		for item in self.chars() {
 			match frequencies.entry(item) {
 				Entry::Vacant(entry) => { entry.insert(1); },
 				Entry::Occupied(mut entry) => *entry.get_mut() += 1,
@@ -29,8 +44,15 @@ mod tests {
 
 	#[test]
 	fn count_letters_test() {
-		let hello: [char; 5] = ['h', 'e', 'l', 'l', 'o'];
+		let hello : &str = "Hello";
 		let freqs = hello.frequencies();
 		assert_eq!(freqs.get(&'l'), Some(&2));
+	}
+
+	#[test]
+	fn count_nums_test() {
+		let nums = [5, 4, 3, 2, 1, 5];
+		let freqs = nums.frequencies();
+		assert_eq!(freqs.get(&5), Some(&2));
 	}
 }
