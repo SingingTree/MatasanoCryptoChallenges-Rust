30dffc6252fefe9ae0970fc29927c1bd2c3e0fcf
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-02-21 11:48:31 +1300

    Added frequency method to FrequencyAnalysiable trait
    Implemented it for IntoIterator and string and added tests

--- src/frequency_analysis.rs
+++ src/frequency_analysis.rs
@@ -5,7 +5,10 @@ use std::iter::IntoIterator;
 trait FrequencyAnalysable {
 	type Item : Ord;
 
+	// Returns a BTreeMap from the items in self to their number of occurrences
 	fn occurrences(self) -> BTreeMap<Self::Item, usize>;
+	// Returns a BTreeMap from the items in self to their frequency%
+	fn frequencies(self) -> BTreeMap<Self::Item, f32>;
 }
 
 fn occurrences_from_iter<I : Iterator>(iter : I) -> BTreeMap<<I as Iterator>::Item, usize>
@@ -20,12 +23,34 @@ where <I as Iterator>::Item: Ord {
     return occurrences;
 }
 
+fn frequencies_from_iter<I : Iterator>(iter : I) -> BTreeMap<<I as Iterator>::Item, f32>
+where <I as Iterator>::Item: Ord {
+    let mut occurrences = BTreeMap::new();
+    let mut i = 0;
+    for item in iter {
+        match occurrences.entry(item) {
+            Entry::Vacant(entry) => { entry.insert(1); }
+            Entry::Occupied(entry) => { *entry.into_mut() += 1; }
+        }
+        i += 1;
+    }
+    let mut frequencies = BTreeMap::new();
+    for (key, value) in occurrences.into_iter() {
+        frequencies.insert(key, (value as f32) / (i as f32));
+    }
+    return frequencies;
+}
+
 impl<II : IntoIterator> FrequencyAnalysable for II
 where <<Self as IntoIterator>::IntoIter as Iterator>::Item: Ord {
     type Item = <<Self as IntoIterator>::IntoIter as Iterator>::Item;
 
     fn occurrences(self) -> BTreeMap<<II as FrequencyAnalysable>::Item, usize> {
         return occurrences_from_iter(self.into_iter());
+    }
+
+    fn frequencies(self) -> BTreeMap<<II as FrequencyAnalysable>::Item, f32> {
+    	return frequencies_from_iter(self.into_iter());
     }
 }
 
@@ -35,6 +60,10 @@ impl<'a> FrequencyAnalysable for &'a str {
 	fn occurrences(self) -> BTreeMap<char, usize> {
 		return occurrences_from_iter(self.chars());
 	}
+
+	fn frequencies(self) -> BTreeMap<char, f32> {
+		return frequencies_from_iter(self.chars());
+	}
 }
 
 
@@ -43,16 +72,30 @@ mod tests {
 	use frequency_analysis::FrequencyAnalysable;
 
 	#[test]
-	fn count_letters_test() {
+	fn string_occurrence_test() {
 		let hello : &str = "Hello";
-		let freqs = hello.occurrences();
-		assert_eq!(freqs.get(&'l'), Some(&2));
+		let occurrences = hello.occurrences();
+		assert_eq!(occurrences.get(&'l'), Some(&2));
 	}
 
+    #[test]
+    fn string_frequency_test() {
+        let hello : &str = "Hello";
+        let freqs = hello.frequencies();
+        assert!(freqs.get(&'o') > Some(&0.19) && freqs.get(&'o') < Some(&0.21));
+    }
+
 	#[test]
-	fn count_nums_test() {
+	fn num_slice_occurrence_test() {
 		let nums = [5, 4, 3, 2, 1, 5];
-		let freqs = nums.occurrences();
-		assert_eq!(freqs.get(&5), Some(&2));
+		let occurrences = nums.occurrences();
+		assert_eq!(occurrences.get(&5), Some(&2));
 	}
+
+    #[test]
+    fn num_slice_frequency_test() {
+        let nums = [5, 4, 3, 2, 5];
+        let freqs = nums.frequencies();
+        assert!(freqs.get(&5) > Some(&0.39) && freqs.get(&5) < Some(&0.41));
+    }
 }
