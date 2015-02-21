534f586471bf3e2646a7db325e2db10e8be181f9
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-02-22 10:49:06 +1300

    Add english letter frequency function
    Add a function that returns a BTreeMap containing the average frequencies
    of letters in Enlgish text (as based on the wikipedia article ^_^)

--- src/frequency_analysis.rs
+++ src/frequency_analysis.rs
@@ -66,6 +66,36 @@ impl<'a> FrequencyAnalysable for &'a str {
 	}
 }
 
+fn english_letter_frequencies() -> BTreeMap<char, f32> {
+    let mut frequencies = BTreeMap::new();
+    frequencies.insert('a', 0.08167);
+    frequencies.insert('b', 0.01167);
+    frequencies.insert('c', 0.02782);
+    frequencies.insert('d', 0.04253);
+    frequencies.insert('e', 0.12702);
+    frequencies.insert('f', 0.02228);
+    frequencies.insert('g', 0.02015);
+    frequencies.insert('h', 0.06094);
+    frequencies.insert('i', 0.06966);
+    frequencies.insert('j', 0.00153);
+    frequencies.insert('k', 0.00772);
+    frequencies.insert('l', 0.04025);
+    frequencies.insert('m', 0.02406);
+    frequencies.insert('n', 0.06749);
+    frequencies.insert('o', 0.07507);
+    frequencies.insert('p', 0.01929);
+    frequencies.insert('q', 0.00095);
+    frequencies.insert('r', 0.05987);
+    frequencies.insert('s', 0.06327);
+    frequencies.insert('t', 0.09056);
+    frequencies.insert('u', 0.02758);
+    frequencies.insert('v', 0.00978);
+    frequencies.insert('w', 0.02360);
+    frequencies.insert('x', 0.00150);
+    frequencies.insert('y', 0.01974);
+    frequencies.insert('z', 0.00074);
+    return frequencies;
+}
 
 #[cfg(test)]
 mod tests {
