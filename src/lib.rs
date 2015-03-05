545105bd26eccb1f0ea4c6d07a92bd34e3f5e5ac
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-03-05 20:51:49 +1300

    First run at repeating xor for string
    First attempt at implementing repeating xor for string. Found the trait
    BitXor which can be used to implement the same thing in a more geneic
    fashion, so should do that in the future.

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ pub mod base64;
 pub mod fixed_xor;
 pub mod frequency_analysis;
 pub mod single_byte_xor;
+pub mod repeating_xor;
--- src/repeating_xor.rs
+++ src/repeating_xor.rs
@@ -1,3 +1,30 @@
 trait RepeatingXorEncodable {
-	fn repeating_xor_encode(self) -> String;
+	fn repeating_xor_encode(self, xor_string : &String) -> Vec<u8>;
+}
+
+// Can't implement as below, because there aren't equality contraints on wheres yet :(
+// impl<II : IntoIterator> RepeatingXorEncodable for II
+// where <<Self as IntoIterator>::IntoIter as Iterator>::Item = u8 {
+// }
+
+// TODO: Can implement this using BitXor trait
+
+impl<'a> RepeatingXorEncodable for &'a String {
+	fn repeating_xor_encode(self, xor_string : &String) -> Vec<u8> {
+		let mut xor_string_byte_cycle = xor_string.as_bytes().iter().cycle();
+		let self_byte_iter = self.as_bytes().iter();
+
+		let mut return_vec : Vec<u8> = Vec::new();
+
+		for byte in self_byte_iter {
+			return_vec.push(byte ^ xor_string_byte_cycle.next().unwrap());
+		}
+
+		return return_vec;
+	}
+}
+
+#[cfg(test)]
+mod tests {
+
 }
