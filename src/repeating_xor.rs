bc6671b8e619084aac967a14c347a0df64d396ba
Bryce van Dyk <bryce@vandyk.net.nz>
2015-03-06 16:37:46 +1300

    Ugly half way through reimplementing repeat xor
    Half way through and got interrupted so upping even though it's busted >_>

--- src/repeating_xor.rs
+++ src/repeating_xor.rs
@@ -1,28 +1,18 @@
+use std::string::String;
+
 trait RepeatingXorEncodable {
-	fn repeating_xor_encode(self, xor_string : &String) -> Vec<u8>;
+	fn repeating_xor_encode(self, rhs : Self) -> Self::Output;
+	type Output;
 }
 
-// Can't implement as below, because there aren't equality contraints on wheres yet :(
-// impl<II : IntoIterator> RepeatingXorEncodable for II
-// where <<Self as IntoIterator>::IntoIter as Iterator>::Item = u8 {
-// }
+impl<I : Iterator> RepeatingXorEncodable for I {}
+	where <Self as Iterator>::Item : BitXor {
 
-// TODO: Can implement this using BitXor trait
-
-impl<'a> RepeatingXorEncodable for &'a String {
-	fn repeating_xor_encode(self, xor_string : &String) -> Vec<u8> {
-		let mut xor_string_byte_cycle = xor_string.as_bytes().iter().cycle();
-		let self_byte_iter = self.as_bytes().iter();
-
-		let mut return_vec : Vec<u8> = Vec::new();
-
-		for byte in self_byte_iter {
-			return_vec.push(byte ^ xor_string_byte_cycle.next().unwrap());
-		}
-
-		return return_vec;
+		repeating_xor_encode<u8>
 	}
 }
+
+
 
 #[cfg(test)]
 mod tests {
