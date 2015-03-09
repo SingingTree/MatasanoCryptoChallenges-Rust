3b628545044a9e8e0b09d2a2d7c6d473219a3925
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-03-09 14:29:13 +1300

    Add repeating XOR test for strings

--- src/repeating_xor.rs
+++ src/repeating_xor.rs
@@ -26,14 +26,24 @@ where <Self as Iterator>::Item : BitXor {
 #[cfg(test)]
 mod tests {
 	use repeating_xor::RepeatingXorEncodable;
+	use rustc_serialize::hex::FromHex;
 
 	#[test]
 	fn test_array_u8_repeating_xor() {
-		let plaintext_vec = [0x00, 0xAA, 0xAA, 0x00];
+		let plaintext_array = [0x00, 0xAA, 0xAA, 0x00];
 		let key = [0x00, 0xAA];
 		let mut expected_output = Vec::new(); 
 		expected_output.push(0x00); expected_output.push(0x00); expected_output.push(0xAA); expected_output.push(0xAA);
 
-		assert_eq!(plaintext_vec.iter().repeating_xor_encode(key.iter()), expected_output);
+		assert_eq!(plaintext_array.iter().repeating_xor_encode(key.iter()), expected_output);
+	}
+
+	#[test]
+	fn test_string_repeating_xor() {
+		let plaintext_string = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+		let key = "ICE";
+		let expected_output = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f".from_hex().unwrap();
+
+		assert_eq!(plaintext_string.as_bytes().iter().repeating_xor_encode(key.as_bytes().iter()), expected_output);
 	}
 }
