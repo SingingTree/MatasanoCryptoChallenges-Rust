8858505639c2ff6c078254a55ea8e60bd002d982
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-03-20 18:58:04 +1300

    Approx eq for f32 and f64

--- src/utility.rs
+++ src/utility.rs
@@ -1,3 +1,19 @@
+use std::num::Float;
+
 pub trait ApproxEquality<T> {
-	fn approx_equal(self, other: &T);
+	fn approx_equal(self, other: &T) -> bool;
+}
+
+impl ApproxEquality<f32> for f32 {
+	fn approx_equal(self, other : &f32) -> bool {
+		let epsilon : f32 = 0.0001;
+		return self - *other < epsilon;
+	}
+}
+
+impl ApproxEquality<f64> for f64 {
+	fn approx_equal(self, other : &f64) -> bool {
+		let epsilon : f64 = 0.0001;
+		return self - *other < epsilon;
+	}
 }
