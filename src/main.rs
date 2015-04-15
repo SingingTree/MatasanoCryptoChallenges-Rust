478c8585f3379d1e6edccda371e4105c3cec40b1
Bryce van Dyk <bryce@vandyk.net.nz>
2015-04-15 16:05:43 +1200

    Further SingleByteXor sort based on control chars
    Further sorting on the results of single byte xor based on if elements
    with a similar frequency have differing numbers of control chars

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub mod fixed_xor;
 pub mod frequency_analysis;
 pub mod single_byte_xor;
 pub mod repeating_xor;
+pub mod utility;
 use rustc_serialize::hex::FromHex;
 use std::borrow::Borrow;
 
--- src/single_byte_xor.rs
+++ src/single_byte_xor.rs
@@ -1,6 +1,8 @@
 use std::cmp::Ordering;
 use std::collections::btree_map::BTreeMap;
+// use std::ascii::AsciiExt;
 use frequency_analysis::{self, FrequencyAnalysable};
+use utility::ApproxEquality;
 //use std::ascii::AsciiExt;
 
 pub fn find_textual_decode_candidates(bytes : &[u8], character_frequencies : &BTreeMap<char, f32>) -> Vec<(String, f32)> {
@@ -10,7 +12,7 @@ pub fn find_textual_decode_candidates(bytes : &[u8], character_frequencies : &BT
 	// let mut characters_by_freq : Vec<(char, f32)> = character_frequencies.into_iter().collect();
 	// characters_by_freq.sort_by(|&(_, a), &(_, b )| if a > b {Ordering::Less} else {Ordering::Greater});
 
-	//Brute force for exploration
+	//Brute force exploration of frequencies
 	let mut possible_decodes : Vec<(String, f32)> = Vec::new();
 	for i in 0..255 {
 		let possible_decode : String = bytes.iter().map(|&b| (b ^ i) as char).collect();
@@ -18,7 +20,24 @@ pub fn find_textual_decode_candidates(bytes : &[u8], character_frequencies : &BT
 		possible_decodes.push((possible_decode, possible_decode_freq_difference));
 	}
 
+	// Sort by frequency
 	possible_decodes.sort_by(|&(_, f1), &(_, f2)| if f1 < f2 {Ordering::Less} else {Ordering::Greater});
+
+	// Sort by special characters
+	possible_decodes.sort_by(|&(ref d1, f1), &(ref d2, f2)|
+		if(f1.approx_equal(f2)) {
+			if frequency_analysis::control_character_frequency(d1.chars()) < frequency_analysis::control_character_frequency(d2.chars()) {
+				Ordering::Less
+			} else if frequency_analysis::control_character_frequency(d1.chars()) > frequency_analysis::control_character_frequency(d2.chars()) {
+				Ordering::Greater
+			} else {
+				Ordering::Equal
+			}
+		} else {
+			Ordering::Equal
+		});
+
+	// sort by number of uppercase letters (should probably filter on freq being close)
 	// possible_decodes.sort_by(|&(ref d1, _), &(ref d2, _)|
 	// 	if d1.to_ascii_lowercase() != d2.to_ascii_lowercase() {
 	// 		Ordering::Equal
