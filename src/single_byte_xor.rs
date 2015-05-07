0ddb56436b9b00ceaa96209fd81cc787331b5127
Bryce van Dyk <bryce@vandyk.net.nz>
2015-05-07 16:06:08 +1200

    Rework SingleByteXor best candidates in collection
    Change the find best single byte XOR candidate in a slice and vec to use a
    hard coded heuristic approach. This could (and probably should) be
    reworked to exposed the paramaters used as arguments so that they can be
    tweaked.
    
    Fixed up some warnings.

--- src/single_byte_xor.rs
+++ src/single_byte_xor.rs
@@ -1,4 +1,3 @@
-use std::collections::btree_map::BTreeMap;
 use std::iter::IntoIterator;
 use std::ops::BitXor;
 use utility;
@@ -23,9 +22,7 @@ impl<'a, II> SingleByteXorDecodable for II
     }
 }
 
-pub fn find_best_decode_candidates_for_slice(bit_strings : &[&[u8]], 
-                                             character_frequencies : &BTreeMap<char, f32>)
-                                             -> Vec<String> {
+pub fn find_best_decode_candidates_for_slice_heuristically(bit_strings : &[&[u8]]) -> Vec<String> {
     let mut best_decode_candidates : Vec<String> = Vec::new();
     for s in bit_strings {
         let mut bit_string_decodes = s.find_all_single_byte_xor_decode_candidates();
@@ -41,9 +38,7 @@ pub fn find_best_decode_candidates_for_slice(bit_strings : &[&[u8]],
     return best_decode_candidates;
 }
 
-pub fn find_best_decode_candidates_for_vec(bit_strings : &Vec<Vec<u8>>, 
-                                           character_frequencies : &BTreeMap<char, f32>)
-                                           -> Vec<String> {
+pub fn find_best_decode_candidates_for_vec_heuristically(bit_strings : &Vec<Vec<u8>>) -> Vec<String> {
     let mut best_decode_candidates : Vec<String> = Vec::new();
     for s in bit_strings {
         let mut bit_string_decodes = s.find_all_single_byte_xor_decode_candidates();
@@ -64,8 +59,8 @@ mod tests {
     use rustc_serialize::hex::FromHex;
     use std::borrow::Borrow;
     use utility;
-    use frequency_analysis::{self, FrequencyAnalysable};
-    use single_byte_xor::{self, SingleByteXorDecodable, find_best_decode_candidates_for_slice};
+    use frequency_analysis::FrequencyAnalysable;
+    use single_byte_xor::{SingleByteXorDecodable, find_best_decode_candidates_for_slice_heuristically};
 
     #[test]
     fn frequencies_of_buffer() {
@@ -90,9 +85,9 @@ mod tests {
         let random_bytes_2 = "f4930be3b09a0fd724ad4e1843e27494289c8b793e4bee12722fef52344aba8fe13e".from_hex().unwrap();
         let random_bytes_3 = "5cc78e04ab8ed6d9bce1d2d971b055e387b5b3414dc165e3f75b3cd957bc34a772d0".from_hex().unwrap();
 
-        let mut encoded_slices : [&[u8]; 4] = [text_bytes.borrow(), random_bytes_1.borrow(), random_bytes_2.borrow(), random_bytes_3.borrow()];
+        let encoded_slices : [&[u8]; 4] = [text_bytes.borrow(), random_bytes_1.borrow(), random_bytes_2.borrow(), random_bytes_3.borrow()];
 
-        let mut decode_candidates = find_best_decode_candidates_for_slice(encoded_slices.borrow(), &frequency_analysis::english_letter_frequencies());
+        let mut decode_candidates = find_best_decode_candidates_for_slice_heuristically(encoded_slices.borrow());
 
         assert_eq!(decode_candidates.remove(0), "Cooking MC's like a pound of bacon");
     }
