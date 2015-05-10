45cadc9d886f422792e3bb2b6d5bfa30ba646f23
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-05-10 16:39:04 +1200

    Remove crate dep for bits, implement hamming for u8
    Not gonna use bits crate for implementation of hamming distance (for now).
    Implemented first run at hamming distance for u8. Implemented a test for
    u8 hamming distance

--- src/utility.rs
+++ src/utility.rs
@@ -40,9 +40,18 @@ pub trait HammingDistancable<T> {
 }
 
 impl<'a> HammingDistancable<&'a u8> for &'a u8 {
-    type Output = i32;
-    fn hamming_distance(self, other: &u8) -> i32 {
-        return 0;
+    type Output = u32;
+    fn hamming_distance(self, other: &u8) -> u32 {
+        let mut distance : u32 = 0;
+        if (self ^ other) & 0x01 > 0 { distance += 1}
+        if ((self ^ other) >> 1) & 0x01 > 0 { distance += 1}
+        if ((self ^ other) >> 2) & 0x01 > 0 { distance += 1}
+        if ((self ^ other) >> 3) & 0x01 > 0 { distance += 1}
+        if ((self ^ other) >> 4) & 0x01 > 0 { distance += 1}
+        if ((self ^ other) >> 5) & 0x01 > 0 { distance += 1}
+        if ((self ^ other) >> 6) & 0x01 > 0 { distance += 1}
+        if ((self ^ other) >> 7) & 0x01 > 0 { distance += 1}
+        return distance;
     }
 }
 
@@ -62,9 +71,10 @@ pub fn sort_string_vec_by_char_freq(strings : &mut Vec<String>, character_freque
 #[cfg(test)]
 mod tests {
     use utility::ApproxEquality;
+    use utility::HammingDistancable;
 
     #[test]
-    fn f32_approx_equal_test() {
+    fn f32_approx_equal() {
         let num1 : f32 = 0.001;
         let num2 : f32 = 0.0010000000001;
 
@@ -73,11 +83,26 @@ mod tests {
     }
 
     #[test]
-    fn f32_approx_not_equal_test() {
+    fn f32_approx_not_equal() {
         let num1 : f32 = 0.001;
         let num2 : f32 = 0.00101;
 
         assert!(!num1.approx_equal(num2));
         assert!(!num2.approx_equal(num1));
+    }
+
+    #[test]
+    fn u8_hamming_distance() {
+        let byte1 : u8 = 0x01;
+        let byte2 : u8 = 0x03;
+
+        assert!(byte1.hamming_distance(&byte2) == 1);
+        assert!(byte2.hamming_distance(&byte1) == 1);
+
+        let byte3 : u8 = 0x01;
+        let byte4 : u8 = 0xFF;
+
+        assert!(byte3.hamming_distance(&byte4) == 7);
+        assert!(byte4.hamming_distance(&byte3) == 7);
     }
 }
