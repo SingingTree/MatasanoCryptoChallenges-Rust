e9c46972d810061c7c4e696f44464edd92846a40
Bryce Van Dyk <bryce@vandyk.net.nz>
2017-08-09 22:29:48 +1200

    Streamline find_edit_distance util, add some tests.

--- src/utility.rs
+++ src/utility.rs
@@ -47,38 +47,35 @@ pub fn sort_string_vec_by_char_freq(strings : &mut Vec<String>, character_freque
     });
 }
 
+// Returns a list of (normalised edit distance, length) where normalised edit distance is
+// the edit distance calculated between [..length] and [length..length*2] bytes and then divided
+// by length.
 pub fn find_normalized_edit_distances(bytes: &[u8]) -> Result<Vec<(f32, usize)>, String> {
     let mut normalised_edit_distance_and_lengths = Vec::new();
 
-    if bytes.len() <= 2 {
-        normalised_edit_distance_and_lengths.push((0f32, 1));
-    } else {
-        let mut edit_distance = bytes[..1].bitwise_hamming_distance(&bytes[1..2]);
-        match edit_distance {
-            Ok(ed) => normalised_edit_distance_and_lengths.push((ed as f32, 1)),
-            Err(e) => return Err("Find repeating xor failed attempting to calulate \
-                                hamming distance on iteration 1 with following \
-                                error\n".to_owned() + e)
+    if bytes.len() < 2 {
+        return Err("Cannot find edit distance for bytes of len: ".to_owned() +
+            &bytes.len().to_string());
+    }
+    for possible_key_len in 1..40 {
+        if possible_key_len > bytes.len() / 2 {
+            break;
         }
-        for possible_key_len in 2..40 {
-            if possible_key_len > bytes.len() / 2 {
-                break;
-            }
-            edit_distance = bytes[..possible_key_len].bitwise_hamming_distance(&bytes[possible_key_len..possible_key_len * 2]);
-            match edit_distance {
-                Ok(ed) => {
-                    let normalised_ed = (ed as f32) / (possible_key_len as f32);
-                    normalised_edit_distance_and_lengths.push((normalised_ed, possible_key_len))
-                },
-                Err(e) => return Err("Find repeating xor failed attempting to calulate \
-                                    hamming distance on iteration ".to_owned() +
-                                    &possible_key_len.to_string() +
-                                    &" with following error\n".to_owned() + e)
-            }
+        let edit_distance = bytes[..possible_key_len].bitwise_hamming_distance(
+            &bytes[possible_key_len..possible_key_len * 2]);
+        match edit_distance {
+            Ok(ed) => {
+                let normalised_ed = (ed as f32) / (possible_key_len as f32);
+                normalised_edit_distance_and_lengths.push((normalised_ed, possible_key_len))
+            },
+            Err(e) => return Err("Find repeating xor failed attempting to calulate \
+                                 hamming distance on iteration ".to_owned() +
+                                 &possible_key_len.to_string() +
+                                 &" with following error\n".to_owned() + e)
         }
     }
     normalised_edit_distance_and_lengths.sort_by(|&(ed1, _), &(ed2, _)|
-        ed2.partial_cmp(&ed1).unwrap());
+        ed1.partial_cmp(&ed2).unwrap());
     for &(ed, len) in normalised_edit_distance_and_lengths.iter() {
         println!("{:?}, {:?}", ed, len);
     }
@@ -88,22 +85,32 @@ pub fn find_normalized_edit_distances(bytes: &[u8]) -> Result<Vec<(f32, usize)>,
 #[cfg(test)]
 mod tests {
     use utility::ApproxEquality;
+    use utility::find_normalized_edit_distances;
 
     #[test]
     fn f32_approx_equal() {
         let num1 : f32 = 0.001;
         let num2 : f32 = 0.0010000000001;
+        let num3 : f32 = 0.00101;
 
         assert!(num1.approx_equal(num2));
         assert!(num2.approx_equal(num1));
+
+        assert!(!num1.approx_equal(num3));
+        assert!(!num3.approx_equal(num1));
     }
 
     #[test]
-    fn f32_approx_not_equal() {
-        let num1 : f32 = 0.001;
-        let num2 : f32 = 0.00101;
+    fn normalized_edit_distances() {
+        let bytes1 = [0x01, 0x02]; // Distance of 2 at len 1
+        let bytes2 = [0x01, 0x02, 0x01, 0x03]; // Distance of 0.5 at len 2
 
-        assert!(!num1.approx_equal(num2));
-        assert!(!num2.approx_equal(num1));
+        let edit_distances1 = find_normalized_edit_distances(&bytes1).unwrap();
+        assert!(edit_distances1[0].0.approx_equal(2f32));
+        assert!(edit_distances1[0].1 == 1);
+
+        let edit_distances2 = find_normalized_edit_distances(&bytes2).unwrap();
+        assert!(edit_distances2[0].0.approx_equal(0.5f32));
+        assert!(edit_distances2[0].1 == 2);
     }
 }
